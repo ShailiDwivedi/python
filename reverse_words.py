9cdd4481fbab523253090d13889629751d234f85
Shaili <shailidwivedi05@gmail.com>
2024-05-27 16:01:41 +0530

    first commit

---
+++ reverse_words.py
@@ -0,0 +1,12 @@
+# Find length of string in python
+s = "shaili dwivedi"
+print(s)
+print(len(s))
+
+# Reverse shailiwords in a given String in Python
+s = s.split(" ")      #this is now list of strings
+s.reverse()
+# print(s)
+
+s = " ".join(s) #list to string
+print(s)
